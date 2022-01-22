2e410c028eff89d46e1aa7d35ee21aeaf56e0748
Helena Martín <helena.martin@alum.esci.upf.edu>
2022-01-22 10:13:29 +0100

    Update on main. Data Loading

--- main.py
+++ main.py
@@ -2,9 +2,35 @@
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def train_epoch(dataloader, model, optimizer, criterion):
+DATA_DIR = "/Users/helenamartin/Desktop/AI-Project/"
 
-    # Train the model
+
+class SDDataLoader(json_path):
+    """
+    Self Driving Prokect Dataset.
+    """
+    self.dataset_annotations = self.annotations_to_csv(json_path)
+    self.dataset = self.generate_dict(self.dataset_annotations, DATA_DIR)
+    self.train,self.val = self.split_dataset(self.dataset_annotations)
+
+    def annotations_to_csv(self, json_path):
+        import pandas as pd
+        csv_data = pd.read_json(json_path)
+        return csv_data
+
+    def generate_dict(self, dataset_Annotations, DATA_DIR):
+        from PIL import Image
+        from torchivison import transforms
+        import glob
+        images = glob.glob(os.path.join(DATA_DIR, '*.jpg'))
+        for image in images:
+            convert_tensor = transforms.ToTensor()
+            images[os.path.basename(image)] = convert_tensor(img)
+            labels[os.path.basename(image)] =
+
+
+
+def train_epoch(dataloader, model, optimizer, criterion):
     train_loss = 0
     for X, y in dataloader:
         optimizer...
@@ -31,7 +57,8 @@ def test_epoch(dataloader: DataLoader, model, criterion):
 
 
 def load_data():
-    df = pd.read_csv("/data/housing.csv")
+    #df = pd.read_csv("/data/housing.csv")
+
     train_df, test_df = train_test_split(df, test_size=0.2)
     train_X, train_y = train_df.drop(["ID", "MEDV"], axis=1), train_df["MEDV"]
     test_X, test_y = test_df.drop(["ID", "MEDV"], axis=1), test_df["MEDV"]
