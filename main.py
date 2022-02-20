0e162b51eb644b19727b4edb2cf5d49c69607ca0
ceciliasr <cecilia.siliato@gmail.com>
2022-02-20 13:56:18 +0100

    Added requirements.txt file and changed LR=0.001

--- main.py
+++ main.py
@@ -43,13 +43,13 @@ def train(jsons_p,imgs_p):
         'num_epochs': 100,
         'batch_size': 10,
         'channels': 3,
-        'learning_rate': 0.000001,
+        'learning_rate': 0.001,
         'classes': len(category_list)
     }
     use_gpu = False
 
     wandb.config = {
-        "learning_rate": 0.000001,
+        "learning_rate": 0.001,
         "epochs": 100,
         "batch_size": 10
     }
