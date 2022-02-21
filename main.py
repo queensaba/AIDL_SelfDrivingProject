ab2d85a263ad71ede66a0400bc07ec2c9f31096b
Helena Martín <helena.martin@alum.esci.upf.edu>
2022-02-21 18:57:05 +0100

    Modified hyperparameters

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def train(jsons_p,imgs_p):
     # Defining hyperparameters:
     hparams = {
         'num_epochs': 100,
-        'batch_size': 30,
+        'batch_size': 5,
         'channels': 3,
         'learning_rate': 0.0001,
         'classes': len(category_list)
@@ -51,7 +51,7 @@ def train(jsons_p,imgs_p):
     wandb.config = {
         "learning_rate": 0.0001,
         "epochs": 100,
-        "batch_size": 30
+        "batch_size": 5
     }
 
     data = \
@@ -61,7 +61,7 @@ def train(jsons_p,imgs_p):
             category_list=category_list,
             split_size=7, # Amount of grid cells
             batch_size=hparams['batch_size'],
-            load_size=30
+            load_size=5
         )
     yolo = YoloV1Model(hparams['channels'],classes=hparams['classes'])
     optimizer = torch.optim.SGD(params=yolo.parameters(), lr=hparams['learning_rate'], momentum=0.9, weight_decay=0.0005)
